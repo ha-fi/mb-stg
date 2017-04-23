c8c02fc58d80ab6df4e2f5515cfcc8999a52cc5c
Hafize Okunakol <e0826496@student.tuwien.ac.at>
2017-04-23 16:11:15 +0200

    Update 2017-04-23T14:11:07.040Z

--- js/bootstrap-datepicker-override.js
+++
@@ -1,6 +0,0 @@
-(function($) {
-  // A datepicker is already defined in other systems more than likely
-  // Restore that functionality and re-assign this one
-  var datepicker    = $.fn.datepicker.noConflict(); // return $.fn.datepicker to previously assigned value
-  $.fn.bsDatepicker = datepicker;                   // give $().bsDatepicker the bootstrap-datepicker functionality
-})(window.jQuery);
