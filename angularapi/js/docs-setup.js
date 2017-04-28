79b213c97e24908bd60192bcf9daca7c698ff0af
Hafize Okunakol <e0826496@student.tuwien.ac.at>
2017-04-28 11:00:36 +0200

    Update 2017-04-28T09:00:24.190Z

---
+++ angularapi/js/docs-setup.js
@@ -0,0 +1,203 @@
+NG_DOCS={
+  "sections": {
+    "ngMineralBay": "angular api"
+  },
+  "pages": [
+    {
+      "section": "ngMineralBay",
+      "id": "mineralBay.directive:btArrowScroll",
+      "shortName": "btArrowScroll",
+      "type": "directive",
+      "moduleName": "mineralBay",
+      "shortDescription": "The btArrowScroll attribute directive will direct all arrow key events to a specific element.",
+      "keywords": "$window activated arrow attribute browsers btarrowscroll careful combine direct directive element events exists functionality key kill long mineralbay modals ng-if ngmineralbay programmatically specific"
+    },
+    {
+      "section": "ngMineralBay",
+      "id": "mineralbay.directive:btAutoSubmit",
+      "shortName": "btAutoSubmit",
+      "type": "directive",
+      "moduleName": "mineralbay",
+      "shortDescription": "btAutoSubmit allows you to auto submit a form whenever the model of a form-field changes.",
+      "keywords": "$scope $timeout allows angular auto autosubmittest bt-auto-submit btautosubmit change changes completion controller delayedvalue directive element expression false fire fired fires firing form form-field function inputvalue milliseconds mineralbay model module myname ng-app ng-bind ng-controller ng-model ng-show ng-submit ngform ngmineralbay ngsubmit number requires scheduled set submit submit-timeout submittimeout subtest supplied thinking timeout true truthy type wait will-load willload"
+    },
+    {
+      "section": "ngMineralBay",
+      "id": "mineralbay.directive:btCenterOnPage",
+      "shortName": "btCenterOnPage",
+      "type": "directive",
+      "moduleName": "mineralbay",
+      "shortDescription": "The btCenterOnPage attribute directive will add a top and left to an element",
+      "keywords": "$timeout $window absolutely add attribute btcenteronpage centered directive element left mineralbay modals ngmineralbay top"
+    },
+    {
+      "section": "ngMineralBay",
+      "id": "mineralbay.directive:btDropdown",
+      "shortName": "btDropdown",
+      "type": "directive",
+      "moduleName": "mineralbay",
+      "shortDescription": "btDropdown allows you to create a dropdown based on an object. The keys of the object",
+      "keywords": "$scope aardvark alert allows angular assigned background-color based bear bt-dropdown btdropdown btn call cases cat chosen christian class controller craig create david ddtranslation default directive div doc-example-live dropdown dropdown-test dropdown-toggle droptest expression ffffff function indicates iterating keys-are-numbers keysarenumbers margin-bottom mark mineralbay module ng-controller ng-model ngmineralbay ngmodel number numvalue numvalues object on-assign onassign prefer representation representations save sean sort sorting stored string stringvalue stringvalues throw translation true ui-select values visual width"
+    },
+    {
+      "section": "ngMineralBay",
+      "id": "mineralbay.directive:btErrorImg",
+      "shortName": "btErrorImg",
+      "type": "directive",
+      "moduleName": "mineralbay",
+      "shortDescription": "The btErrorImg directive will replace the src of an img tag if the original src fails to load.",
+      "keywords": "backup bt-error-img bterrorimg directive fails image images img jpg load mineralbay module ngmineralbay original properties replace src tag url"
+    },
+    {
+      "section": "ngMineralBay",
+      "id": "mineralbay.directive:btFocusOn",
+      "shortName": "btFocusOn",
+      "type": "directive",
+      "moduleName": "mineralbay",
+      "shortDescription": "The btFocusOn attribute directive will focus on an input when the expression provided",
+      "keywords": "$timeout attribute btfocuson directive element evaluated expression false falsey focus guiding helpful input mineralbay ngmineralbay provided true truthy user"
+    },
+    {
+      "section": "ngMineralBay",
+      "id": "mineralbay.directive:btFormatMoney",
+      "shortName": "btFormatMoney",
+      "type": "directive",
+      "moduleName": "mineralbay",
+      "shortDescription": "The btFormatMoney attribute directive will format the ngModel of the element it",
+      "keywords": "$window $xxx attribute btformatmoney currency decimal directive element format mineralbay model ngmineralbay ngmodel number stored view xxx"
+    },
+    {
+      "section": "ngMineralBay",
+      "id": "mineralbay.directive:btIFrame",
+      "shortName": "btIFrame",
+      "type": "directive",
+      "moduleName": "mineralbay",
+      "shortDescription": "The btIFrame attribute directive allows the user to embed an iFrame with the ability",
+      "keywords": "$window ability allows angular argument attribute btiframe call called calling close closed closeframe closing code communicate directive domain embed expression function global height iframe implement intended internally mineralbay ngmineralbay number registerclose representing sets src takes uri user width wishes"
+    },
+    {
+      "section": "ngMineralBay",
+      "id": "mineralbay.directive:btInfiniteScrollElement",
+      "shortName": "btInfiniteScrollElement",
+      "type": "directive",
+      "moduleName": "mineralbay",
+      "shortDescription": "The btInfiniteScrollElement attribute directive allows the user to trigger an expression",
+      "keywords": "$rootscope $timeout allows attribute bottom btinfinitescrollelement close directive element evaluate expression illusion infinite infinitescrolldistance mineralbay ngmineralbay number representing scroll scrolled trigger triggered user"
+    },
+    {
+      "section": "ngMineralBay",
+      "id": "mineralbay.directive:btLockBody",
+      "shortName": "btLockBody",
+      "type": "directive",
+      "moduleName": "mineralbay",
+      "shortDescription": "The btLockBody attribute directive will prevent the body from scrolling whenever",
+      "keywords": "$document $window angular attached attribute body bootstrap btlockbody directive element exists invoking lock locked mineralbay modal ng-if ngmineralbay older prevent scrolling time version"
+    },
+    {
+      "section": "ngMineralBay",
+      "id": "mineralbay.directive:btNgModelOnBlur",
+      "shortName": "btNgModelOnBlur",
+      "type": "directive",
+      "moduleName": "mineralbay",
+      "shortDescription": "The btNgModelOnBlur attribute directive when used with an input will only update the",
+      "keywords": "angular attribute btngmodelonblur directive enter focus input key left mineralbay ngmineralbay ngmodel ngmodeloptions pressed redundant update user"
+    },
+    {
+      "section": "ngMineralBay",
+      "id": "mineralbay.directive:btNumber",
+      "shortName": "btNumber",
+      "type": "directive",
+      "moduleName": "mineralbay",
+      "shortDescription": "The btNumber attribute directive formats the ngModel&#39;s data to only",
+      "keywords": "allow attribute btnumber data directive don formats good inputs mineralbay ngmineralbay ngmodel number numbers type user"
+    },
+    {
+      "section": "ngMineralBay",
+      "id": "mineralbay.directive:btPager",
+      "shortName": "btPager",
+      "type": "directive",
+      "moduleName": "mineralbay",
+      "shortDescription": "The btPager directive is a wrapper of the ui-bootstrap pagination directive that keeps",
+      "keywords": "allowed based btpager current currentpage directive disable enable items itemsperpage mineralbay ngmineralbay number paginate pagination programmatically provide template totalitems track ui-bootstrap wrapper wrapping"
+    },
+    {
+      "section": "ngMineralBay",
+      "id": "mineralbay.directive:btScore",
+      "shortName": "btScore",
+      "type": "directive",
+      "moduleName": "mineralbay",
+      "shortDescription": "The btScore directive represents a score component from Mineralbay.  It will keep the color",
+      "keywords": "average based btscore classes color component default directive element excellent good high include lg low medium mineralbay ngmineralbay represents score size sizes sm valid xs"
+    },
+    {
+      "section": "ngMineralBay",
+      "id": "mineralbay.directive:btScrollbar",
+      "shortName": "btScrollbar",
+      "type": "directive",
+      "moduleName": "mineralbay",
+      "shortDescription": "The btScrollbar directive adds a simulated scrollbar to any element.  It wraps the jQuery baron library.",
+      "keywords": "adds baron btscrollbar directive ea element jquery library mineralbay ngmineralbay scrollbar simulated wraps"
+    },
+    {
+      "section": "ngMineralBay",
+      "id": "mineralbay.directive:btSelectRange",
+      "shortName": "btSelectRange",
+      "type": "directive",
+      "moduleName": "mineralbay",
+      "shortDescription": "The btSelectRange element directive wraps the AngularUI&#39;s uiSelect directive.",
+      "keywords": "$filter acceptable add allows angularui array bootstrap btnclass btselectrange button change class control currency current directive display dropdown dropdowns element empty ensures enters format function input item items list max maximum maxplaceholder min mineralbay minimum minplaceholder money ngmineralbay non-provided number numbers placeholder provided range rangetype satisfactory select selecting string style text translate type types uiselect user users values wraps"
+    },
+    {
+      "section": "ngMineralBay",
+      "id": "mineralbay.directive:lazyPen",
+      "shortName": "lazyPen",
+      "type": "directive",
+      "moduleName": "mineralbay",
+      "shortDescription": "Lazy loads a CodePen iframe",
+      "keywords": "codepen directive iframe lazy loads mineralbay ngmineralbay"
+    }
+  ],
+  "apis": {
+    "ngMineralBay": true
+  },
+  "__file": "_FAKE_DEST_/js/docs-setup.js",
+  "__options": {
+    "startPage": "/ngMineralBay",
+    "scripts": [
+      "js/marked.js",
+      "js/prettify.js",
+      "js/mineralbay.js",
+      "js/mineralbay-angular.js",
+      "js/angular-animate.min.js"
+    ],
+    "styles": [
+      "//fonts.googleapis.com/css?family=Roboto:500,300,700,400italic,400",
+      "css/mineralbay.css"
+    ],
+    "title": "",
+    "html5Mode": false,
+    "editExample": true,
+    "navTemplate": "views/partials/ngdocs-nav.html",
+    "navContent": "<ul id=\"pl-docs-nav\" class=\"nav navbar-nav\">\n  <li><a href=\"../index.html\">Introduction</a></li>\n  <li><a href=\"../css.html\">CSS</a></li>\n  <li><a href=\"../components.html\">Components</a></li>\n  <li><a href=\"../javascript.html\">JavaScript</a></li>\n  <li><a href=\"../angular.html\">Angular</a></li>\n  <!-- <li><a href=\"../angularapi\">API</a></li> -->\n</ul>\n<script type=\"text/javascript\">\n  $(document).ready(function() {\n  \t$('ul.breadcrumb').css('display', 'none');\n  });\n</script>",
+    "navTemplateData": {},
+    "loadDefaults": {
+      "angular": false,
+      "angularAnimate": false,
+      "marked": true,
+      "prettify": true
+    },
+    "image": "img/mineralbay-logo.png",
+    "imageLink": "/index.html",
+    "titleLink": "/index.html"
+  },
+  "html5Mode": false,
+  "editExample": true,
+  "startPage": "/ngMineralBay",
+  "scripts": [
+    "marked.js",
+    "prettify.js",
+    "mineralbay.js",
+    "mineralbay-angular.js",
+    "angular-animate.min.js"
+  ]
+};
